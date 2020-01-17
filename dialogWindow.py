3da334bf7665104d53c6f7062f3b01b46ccb3571
alexandr <zelensk1973@gmail.com>
2020-01-17 17:47:46 +0200

    Додав скріншот з ws вікном 22

---
+++ dialogWindow.py
@@ -0,0 +1,9 @@
+import wx
+
+class SubclassDialog(wx.Dialog):
+    def __init__(self):              #РёРЅРёС†РёРёСЂСѓРµРј РґРёР°Р»РѕРі
+        wx.Dialog.__init__(self, None, -1, 'Dialog Subclass',size=(120, 150),
+                           style=wx.CAPTION|wx.STAY_ON_TOP|wx.TAB_TRAVERSAL)
+        okButton = wx.Button(self, wx.ID_OK, "OK", pos=(2, 2), size=(100, 32))
+        okButton.SetDefault()
+        cancelButton = wx.Button(self, wx.ID_CANCEL, "Cancel",pos=(2, 40), size=(100, 32))
